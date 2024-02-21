03aa166c861a9bb51e6977a881ee9fe7c0015867
WinterCT <160752384+WinterCT@users.noreply.github.com>
2024-02-21 15:44:42 +0100

    Add files via upload

---
+++ Recruitment/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/images/CyberTankz.png" type="image/x-icon">
+    <div class="header-menu">
+         <ul>
+           <a href="/">Home</a>
+           <a href="https://cybertankz.com/recruitment">Recruitment</a
+         </ul>  
+       </div>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background: #121725; 
+            display: flex;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        p {
+            color: #ccc;
+            font-size: 250px;
+            font-family: monospace;
+        }
+
+        ul {
+            list-style-type: none;
+        }
+
+        .header-menu {
+            transition: .3s;
+            color: #ccc;
+            background: rgb(0 39 59 / 30%);
+            border-bottom: 1px solid rgba(125, 157, 186, .3);
+            position: fixed;
+            width: 100%;
+            height: 60px;
+            z-index: 2;
+            text-align: center;
+        }
+
+        a {
+            margin-right: 1em;
+            transition: 0.3s;
+            color: #fff;
+            font-size: 24px;
+            font-family: arial;
+            text-decoration: none;
+            color: inherit;
+        }
+
+        ::marker {
+            display: none;
+        }
+
+        .box {
+            max-width: 600px;
+            padding: 20px;
+            background: linear-gradient(0deg, rgb(0 65 15 / 50%), transparent), linear-gradient(180deg, rgb(214 255 0 / 20%), transparent), url(https://helper.tankionline.com/upload/admins/banners/Banner_for_SM.jpg);
+            background-size: cover;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            color: #fff;
+            display: flex;
+            align-items: center;
+        }
+
+        .avatar {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            margin-right: 20px;
+        }
+
+        h2 {
+            font-weight: bold;
+            font-size: 24px;
+        }
+
+        .subtitle {
+            font-size: 16px;
+            line-height: 1.5;
+            margin-top: 10px;
+        }
+
+        .recruitment {
+            margin-top: 20px;
+            font-weight: bold;
+        }
+    </style>
+    <title>CyberTankz</title>
+</head>
+<body>
+    
+
+
+<p>SOON</p></body></html>
---
+++ Recruitment/recruitment/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/images/CyberTankz.png" type="image/x-icon">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background: #121725; 
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .box {
+            max-width: 600px;
+            padding: 20px;
+            background: linear-gradient(0deg, rgb(0 65 15 / 50%), transparent), linear-gradient(180deg, rgb(214 255 0 / 20%), transparent), url(/recruitment/images/socialmedia.png);
+            background-size: cover;
+            background-position: center;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            color: #fff;
+            text-shadow: 2px 2px 4px rgb(0 0 0 / 50%);
+            display: flex;
+            align-items: center;
+        }
+
+        .avatar {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            margin-right: 20px;
+        }
+
+        h2 {
+            font-weight: bold;
+            font-size: 24px;
+        }
+
+        a {
+            text-decoration: none;
+            color: inherit;
+        }
+
+        .subtitle {
+            font-size: 16px;
+            line-height: 1.5;
+            margin-top: 10px;
+        }
+
+        .recruitment {
+            margin-top: 20px;
+            font-weight: bold;
+        }
+    </style>
+    <title>CyberTankz Recruitment</title>
+</head>
+<body>
+    <a href="/recruitment/position/01" class="box">
+        <img class="avatar" src="/recruitment/images/DiscordModeratorAvatar.png" alt="Avatar">
+        <div>
+            <h2>Discord Moderator</h2>
+            <p class="subtitle">Discord moderator plays a crucial role in maintaining order, safety, and a positive atmosphere on the server. Their main tasks include monitoring chat and channels, removing inappropriate content, and responding to violations of community guidelines.</p>
+            <p class="recruitment">Recruitment: Open</p>
+        </div>
+        </a>
+
+</body>
+</html>
